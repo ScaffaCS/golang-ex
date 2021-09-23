cb03d725d9b9d37c4c3890bf0140eb011b4e96a1
Craig Simpson <csimpson1985@gmail.com>
2021-09-23 13:45:17 +0100

    Added Issy

--- hello_openshift.go
+++ hello_openshift.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift Modified!"
+		response = "Hello OpenShift Issy!"
 	}
 
 	fmt.Fprintln(w, response)
